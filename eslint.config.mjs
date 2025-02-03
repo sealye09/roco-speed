3eb05ea2960c8f0622732dd121539407d52280ab
Seal <yejunhai2001@gmail.com>
2025-02-03 17:25:00 +0800

    dependence

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import perfectionist from 'eslint-plugin-perfectionist';
+import tailwind from 'eslint-plugin-tailwindcss';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +11,22 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const nextConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
 ];
 
-export default eslintConfig;
+const config = [
+  ...nextConfig,
+  ...tailwind.configs['flat/recommended'],
+  {
+    plugins: {
+      perfectionist,
+    },
+    rules: {
+      'perfectionist/sort-imports': 'error',
+      'tailwindcss/no-custom-classname': 'off',
+    },
+  },
+];
+
+export default config;
